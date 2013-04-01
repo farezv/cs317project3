0056a5ea249dc302a725b1a156368c1bab15b0de
farezv <farezvad@gmail.com>
2013-04-01 16:33:53 -0700

    main.c -- added data structures for socket con

--- main.c
+++ main.c
@@ -9,6 +9,31 @@ void *serve_client(void *ptr) {
 
 int main() {
     // ...
+
+    // Lines 13-32 from beej's 5.1
+int status;
+struct addrinfo hints;
+struct addrinfo *servinfo;  // will point to list of struct addrinfos each
+                            // which contain a struct socket address.
+
+memset(&hints, 0, sizeof hints); // make sure the struct is empty
+hints.ai_family = AF_UNSPEC;     // don't care IPv4 or IPv6
+hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
+hints.ai_flags = AI_PASSIVE;     // fill in my IP for me
+
+
+if ((status = getaddrinfo(NULL, "5555", &hints, &servinfo)) != 0) {
+    fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+    exit(1);
+}
+
+// servinfo now points to a linked list of 1 or more struct addrinfos
+
+// ... do everything until you don't need servinfo anymore ....
+
+
+
+
     // Required variables
     int client_fd;    // Socket returned by accept
     pthread_t thread; // Thread to be created
@@ -17,4 +42,7 @@ int main() {
     // Detaches the thread. This means that, once the thread finishes, it is destroyed.
     pthread_detach(thread);
     // ...
+
+freeaddrinfo(servinfo); // free the linked-list here
+
 }
