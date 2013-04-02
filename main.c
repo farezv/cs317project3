79e93bcd8b2f3253b76693555c7a88ab8afa7056
farezv <farezvad@gmail.com>
2013-04-01 20:57:37 -0700

    Changed main() to accept socket connections
    main() is updated with server code from
    http://beej.us/guide/bgnet/output/html/multipage/index.html to handle a
    queue of incoming socket connections

--- main.c
+++ main.c
@@ -1,48 +1,131 @@
 #include <stdint.h>
 #include <pthread.h>
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+#define PORT "3490"  // the port users will be connecting to
+
+#define BACKLOG 10     // how many pending connections queue will hold
+
+/*
+* serve_client() given by CS317 instructors
+*/
 void *serve_client(void *ptr) {
   // Converts ptr back to integer.
   int client_fd = (int) (intptr_t) ptr;
   // ...
 }
 
-int main() {
-    // ...
+// get sockaddr, IPv4 or IPv6: from Beej's guide
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
 
-    // Lines 13-32 from beej's 5.1
-int status;
-struct addrinfo hints;
-struct addrinfo *servinfo;  // will point to list of struct addrinfos each
-                            // which contain a struct socket address.
-
-memset(&hints, 0, sizeof hints); // make sure the struct is empty
-hints.ai_family = AF_UNSPEC;     // don't care IPv4 or IPv6
-hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
-hints.ai_flags = AI_PASSIVE;     // fill in my IP for me
-
-
-if ((status = getaddrinfo(NULL, "5555", &hints, &servinfo)) != 0) {
-    fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
-    exit(1);
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
-// servinfo now points to a linked list of 1 or more struct addrinfos
+/* Beej's main()
+*  
+*/
+int main(void)
+{
+    int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
+    struct addrinfo hints, *servinfo, *p;
+    struct sockaddr_storage their_addr; // connector's address information
+    socklen_t sin_size;
+    struct sigaction sa;
+    int yes=1;
+    char s[INET6_ADDRSTRLEN];
+    int rv;
 
-// ... do everything until you don't need servinfo anymore ....
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE; // use my IP
+
+    if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+    // loop through all the results and bind to the first we can
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -1) {
+            perror("server: socket");
+            continue;
+        }
+
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+                sizeof(int)) == -1) {
+            perror("setsockopt");
+            exit(1);
+        }
+
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("server: bind");
+            continue;
+        }
+
+        break;
+    }
+
+    if (p == NULL)  {
+        fprintf(stderr, "server: failed to bind\n");
+        return 2;
+    }
+
+    freeaddrinfo(servinfo); // all done with this structure
+
+    if (listen(sockfd, BACKLOG) == -1) {
+        perror("listen");
+        exit(1);
+    }
+
+    printf("server: waiting for connections...\n");
+
+    while(1) {  // main accept() loop
+        sin_size = sizeof their_addr;
+        new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+        if (new_fd == -1) {
+            perror("accept");
+            continue;
+        }
+        // Reading socket addresses and printing them out.
+        inet_ntop(their_addr.ss_family,
+            get_in_addr((struct sockaddr *)&their_addr),
+            s, sizeof s);
+        printf("server: got connection from %s\n", s);
+
+        // pthread code given by CS317 instructors
+        
+        // Required variables
 
 
+        int client_fd;    // Socket returned by accept
+        pthread_t thread; // Thread to be created
+        // Creates the thread and calls the function serve_client.
+        pthread_create(&thread, NULL, serve_client, (void *) (intptr_t) client_fd);
+        // Detaches the thread. This means that, once the thread finishes, it is destroyed.
+        pthread_detach(thread);
+        // ...
 
+        close(new_fd);  // parent doesn't need this
+    }
 
-    // Required variables
-    int client_fd;    // Socket returned by accept
-    pthread_t thread; // Thread to be created
-    // Creates the thread and calls the function serve_client.
-    pthread_create(&thread, NULL, serve_client, (void *) (intptr_t) client_fd);
-    // Detaches the thread. This means that, once the thread finishes, it is destroyed.
-    pthread_detach(thread);
-    // ...
-
-freeaddrinfo(servinfo); // free the linked-list here
-
+    return 0;
 }
